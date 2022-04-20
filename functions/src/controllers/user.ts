b3e87dbc3cfdb27689e3da59a1283ca1af7355a1
Phahladira-Moja <phahladira@patala.co.za>
2022-04-20 12:02:55 +0200

    Updated update user and user preferences

--- functions/src/controllers/user.ts
+++ functions/src/controllers/user.ts
@@ -10,55 +10,35 @@ const signUp = async (req:functions.https.Request, res: functions.Response) => {
         const binu = JSON.parse(Array.isArray(jsonString) ? jsonString[0] : jsonString);
         const uid = binu.did;
 
-        const nameIndex = req.url.indexOf(util.FunctionsConstants.Name);
-        const genderIndex = req.url.indexOf(util.FunctionsConstants.Gender);
-        const lookingForIndex = req.url.indexOf(util.FunctionsConstants.LookingFor);
-        const ageIndex = req.url.indexOf(util.FunctionsConstants.Age);
-        const locationIndex = req.url.indexOf(util.FunctionsConstants.Location);
 
-        let nameString = req.url.substring(nameIndex+util.FunctionsConstants.Name.length+1, genderIndex-1);
-        nameString = nameString.replace(util.FunctionsConstants.PlusSign, ' ');
-
-
-        const genderString = req.url.substring(genderIndex+util.FunctionsConstants.Gender.length+1, lookingForIndex-1);
-
-        const lookingForString = req.url.substring(lookingForIndex+util.FunctionsConstants.LookingFor.length+1, ageIndex-1);
-
-        const ageString = req.url.substring(ageIndex+util.FunctionsConstants.Age.length+1, locationIndex-1);
-
-        let locationString = req.url.substring(locationIndex+util.FunctionsConstants.Location.length+1);
-        locationString = locationString.replace(util.FunctionsConstants.PlusSign, ' ');
-
-
-        const ageNumber = Number(ageString);
+        const ageNumber = Number(req.query.age);
 
         const ageMin = ageNumber - 5;
         const ageMax = ageNumber + 5;
 
         admin.firestore().collection(util.FunctionsConstants.Users).doc(uid).set({
-            age: ageString,
-            name: nameString,
+            age: req.query.age,
+            name: req.query.name,
             bio: '',
-            gender: genderString,
+            gender: req.query.gender,
             images: [],
             isVerified: false,
+            points: 0,
             verifiedPaymentsId: '',
-            location: locationString,
+            location: req.query.location,
         });
 
         admin.firestore().collection(util.FunctionsConstants.Preferences).doc(uid).set({
-            gender: lookingForString,
-            location: locationString,
+            gender: req.query.lookingFor,
+            location: req.query.location,
             ageMin: ageMin.toString(),
             ageMax: ageMax.toString(),
         });
 
         res.status(200).send(util.SuccessMessages.SuccessMessage);
-        return;
     } catch (error) {
         console.error(util.ErrorMessages.ErrorText, error);
         res.status(404).send(util.ErrorMessages.UnexpectedExrror);
-        return;
     }
 };
 
@@ -70,17 +50,12 @@ const getUserInformation = async (req:functions.https.Request, res: functions.Re
     try {
         const userDocument = await admin.firestore().collection(util.FunctionsConstants.Users).doc(uid).get();
 
-        if (!userDocument.exists) {
-            res.status(400).send(util.ErrorMessages.NoUserError);
-            return;
-        }
+        if (!userDocument.exists) res.status(400).send(util.ErrorMessages.NoUserError);
 
         res.status(200).send(userDocument.data());
-        return;
     } catch (error) {
         console.error(util.ErrorMessages.ErrorText, error);
         res.status(404).send(util.ErrorMessages.UnexpectedExrror);
-        return;
     }
 };
 
@@ -90,45 +65,21 @@ const updateUserInformation = async (req:functions.https.Request, res: functions
         const binu = JSON.parse(Array.isArray(jsonString) ? jsonString[0] : jsonString);
         const uid = binu.did;
 
-        const nameIndex = req.url.indexOf(util.FunctionsConstants.Name);
-        const ageIndex = req.url.indexOf(util.FunctionsConstants.Age);
-        const locationIndex = req.url.indexOf(util.FunctionsConstants.Location);
-        const bioIndex = req.url.indexOf(util.FunctionsConstants.Bio);
-        const lookingForIndex = req.url.indexOf(util.FunctionsConstants.LookingFor);
+        const updateObject = {};
 
-        let nameString = req.url.substring(nameIndex+util.FunctionsConstants.Name.length+1, ageIndex-1);
-        nameString = nameString.replace(util.FunctionsConstants.PlusSign, ' ');
+        if (req.query.age != null) Object.assign(updateObject, {age: req.query.age});
 
-        const ageString = req.url.substring(ageIndex+util.FunctionsConstants.Age.length+1, locationIndex-1);
+        if (req.query.bio != null) Object.assign(updateObject, {bio: req.query.bio});
 
-        let locationString = req.url.substring(locationIndex+util.FunctionsConstants.Location.length+1, bioIndex-1);
-        locationString = locationString.replace(util.FunctionsConstants.PlusSign, ' ');
+        if (req.query.name != null) Object.assign(updateObject, {name: req.query.name});
 
+        if (req.query.location != null) Object.assign(updateObject, {location: req.query.location});
 
-        let bioString = req.url.substring(bioIndex+util.FunctionsConstants.Bio.length+1, lookingForIndex-1);
-        bioString = bioString.replace(util.FunctionsConstants.PlusSign, ' ');
-
-
-        const lookingForString = req.url.substring(lookingForIndex+util.FunctionsConstants.LookingFor.length+1);
-
-        const userDocument = await admin.firestore().collection(util.FunctionsConstants.Users).doc(uid).get();
-
-        if (!userDocument.exists) {
-            res.status(400).send(util.ErrorMessages.NoUserError);
-            return;
-        }
-
-        await admin.firestore().collection(util.FunctionsConstants.Users).doc(uid).update({
-            age: ageString == '-1' ? userDocument.data()?.age : ageString,
-            bio: bioString == '-1' ? userDocument.data()?.bio : bioString,
-            name: nameString == '-1' ? userDocument.data()?.name : nameString,
-            location: locationString == '-1' ? userDocument.data()?.location : locationString,
-            lookingFor: lookingForString == '-1' ? userDocument.data()?.lookingFor : lookingForString,
-        });
+        await admin.firestore().collection(util.FunctionsConstants.Users).doc(uid).update(updateObject);
+        res.status(200).send(util.SuccessMessages.SuccessMessage);
     } catch (error) {
         console.error(util.ErrorMessages.ErrorText, error);
         res.status(404).send(util.ErrorMessages.UnexpectedExrror);
-        return;
     }
 };
 
@@ -160,37 +111,22 @@ const updateUserPreferences = async (req:functions.https.Request, res: functions
         const binu = JSON.parse(Array.isArray(jsonString) ? jsonString[0] : jsonString);
         const uid = binu.did;
 
-        const ageMinIndex = req.url.indexOf(util.FunctionsConstants.AgeMin);
-        const ageMaxIndex = req.url.indexOf(util.FunctionsConstants.AgeMax);
-        const genderIndex = req.url.indexOf(util.FunctionsConstants.Gender);
-        const locationIndex = req.url.indexOf(util.FunctionsConstants.Location);
+        const updateObject = {};
 
-        const ageMinString = req.url.substring(ageMinIndex+util.FunctionsConstants.AgeMin.length+1, ageMaxIndex-1);
+        if (req.query.ageMin != null) Object.assign(updateObject, {ageMin: req.query.ageMin});
 
-        const ageMaxString = req.url.substring(ageMaxIndex+util.FunctionsConstants.AgeMax.length+1, genderIndex-1);
+        if (req.query.ageMax != null) Object.assign(updateObject, {ageMax: req.query.ageMax});
 
-        const genderString = req.url.substring(genderIndex+util.FunctionsConstants.Gender.length+1, locationIndex-1);
+        if (req.query.gender != null) Object.assign(updateObject, {gender: req.query.gender});
 
-        let locationString = req.url.substring(locationIndex+util.FunctionsConstants.Location.length+1);
-        locationString = locationString.replace(util.FunctionsConstants.PlusSign, ' ');
+        if (req.query.location != null) Object.assign(updateObject, {location: req.query.location});
 
-        const userPreferenceDocument = await admin.firestore().collection(util.FunctionsConstants.Preferences).doc(uid).get();
+        await admin.firestore().collection(util.FunctionsConstants.Preferences).doc(uid).update(updateObject);
 
-        if (!userPreferenceDocument.exists) {
-            res.status(400).send(util.ErrorMessages.NoUserError);
-            return;
-        }
-
-        await admin.firestore().collection(util.FunctionsConstants.Preferences).doc(uid).update({
-            ageMin: ageMinString == '-1' ? userPreferenceDocument.data()?.ageMax : ageMinString,
-            ageMax: ageMaxString == '-1' ? userPreferenceDocument.data()?.ageMax : ageMaxString,
-            gender: genderString == '-1' ? userPreferenceDocument.data()?.gender : genderString,
-            location: locationString == '-1' ? userPreferenceDocument.data()?.location : locationString,
-        });
+        res.status(200).send(util.SuccessMessages.SuccessMessage);
     } catch (error) {
         console.error(util.ErrorMessages.ErrorText, error);
         res.status(404).send(util.ErrorMessages.UnexpectedExrror);
-        return;
     }
 };
 
